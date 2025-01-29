3111cd2cbea25a7811c213a06aa5f4d94ea26a2f
Ivan Baha <ivan.baha@altran.com>
2025-01-29 15:05:10 +0000

    feat: refactor

--- src/env.service.ts
+++ src/env.service.ts
@@ -9,12 +9,12 @@ export class EnvService {
   }
 
   private stopProcess(message: string): never {
-    // eslint-disable no-console
+    // eslint-disable-next-line no-console
     console.log("\x1b[31m%s\x1b[0m", message);
     process.exit(1);
   }
 
-  private checkEnvExisting(envName: string): boolean {
+  private checkEnvExisting(envName: string): boolean | never {
     if (!process.env.hasOwnProperty(envName)) this.stopProcess(`Missing '${envName}' environment variable`);
     return true;
   }
@@ -40,26 +40,28 @@ export class EnvService {
   }
 
   /**
-   * Returns a value of required environment variable.
-   * Throws an error if it was not set.
+   * Retrieves a string value of required environment variable.
+   * Stops the entire Nodejs process in case the ENV is missing.
    * @param envName - The name of the environment variable.
-   * @param allowedValues - The list of allowed values for current environment variable.
+   * @param allowedValues - The list of allowed values for the environment variable.
+   * @returns The value of the ENV.
    */
   getRequiredEnv(envName: string): string;
   getRequiredEnv(envName: string, allowedValues: string[]): string;
   getRequiredEnv(envName: string, allowedValues?: string[]): string {
     this.checkEnvExisting(envName);
+
     if (allowedValues?.length) this.checkIfEnvHasAllowedValue(envName, process.env[envName], allowedValues);
 
     return String(process.env[envName]);
   }
 
   /**
-   * Returns a string value of the optional environment variable.
-   * Returns the default value or undefined if the environment variable was not set.
+   * Retrieves a string value of an optional environment variable.
    * @param envName - The name of the environment variable.
-   * @param defaultValue - The value returned if the environment variable was not set.
-   * @param allowedValues - The list of allowed values for current environment variable.
+   * @param defaultValue - The default value for the case when the environment variable is not set.
+   * @param allowedValues - The list of allowed values for validation environment variable by.
+   * @returns The value of the ENV, the default value, or undefined.
    */
   getOptionalEnv(envName: string): string | undefined;
   getOptionalEnv(envName: string, defaultValue: string): string;
@@ -72,11 +74,13 @@ export class EnvService {
   ): string | undefined {
     if (Array.isArray(defaultValueOrAllowedValues)) {
       const envValue = process.env[envName];
+
       this.checkIfEnvHasAllowedValue(envName, envValue, defaultValueOrAllowedValues);
 
       return envValue;
     } else {
       const envValue = process.env[envName] || defaultValueOrAllowedValues;
+
       if (allowedValues?.length) this.checkIfEnvHasAllowedValue(envName, envValue, allowedValues);
 
       return envValue;
@@ -84,44 +88,53 @@ export class EnvService {
   }
 
   /**
-   * Returns a decimal numeric value of the optional environment variable.
-   * Returns the default value if the environment variable was not set.
-   * @param envName - The name of the environment variable.
-   * @param defaultValue - The value returned if the environment variable was not set.
-   */
-  getNumericEnv(envName: string): number | undefined;
-  getNumericEnv(envName: string, defaultValue: number): number;
-  getNumericEnv(envName: string, defaultValue?: number): number | undefined {
-    const envVal = String(process.env[envName]);
-    return /^[0-9_]+$/.test(envVal) ? Number(envVal.replace("_", "")) : defaultValue;
-  }
-
-  /**
    * Retrieves a decimal numeric value of the required environment variable.
-   * Throws an error if it is not set.
+   * Stops the entire Nodejs process in case the ENV is missing.
    * @param envName - The name of the environment variable.
    * @returns Decimal numeric value of the required environment variable.
    */
   getRequiredNumericEnv(envName: string): number {
     const envVal = this.getRequiredEnv(envName);
-    if (envVal === "skip_variable") return 1;
+
     if (!/^[0-9_]+$/.test(envVal)) this.stopProcess(`Variable '${envName}' is not of number type.`);
-    return Number(envVal.replace("_", ""));
+
+    return Number(envVal.replace(/_/g, ""));
   }
 
   /**
-   * Retrieves the value of boolean environment variable.
+   * Retrieves a decimal numeric value of the optional environment variable.
    * @param envName - The name of the environment variable.
-   * @returns True if environment variable was set and its value equals 'true'. Otherwise returns false.
+   * @param defaultValue - The default value for the case when the environment variable is not set.
+   * @returns The value of the ENV, the default value, or undefined.
    */
-  getBooleanEnv(envName: string): boolean {
-    return process.env[envName] === "true";
+  getOptionalNumericEnv(envName: string): number | undefined;
+  getOptionalNumericEnv(envName: string, defaultValue: number): number;
+  getOptionalNumericEnv(envName: string, defaultValue?: number): number | undefined {
+    const envVal = String(process.env[envName]);
+
+    return /^[0-9_]+$/.test(envVal) ? Number(envVal.replace(/_/g, "")) : defaultValue;
   }
 
   /**
-   * Returns true if environment variable was set and its value equals 'true' or false if it has different value.
+   * Retrieves the value of required boolean environment variable.
+   * Stops the entire Nodejs process in case the ENV is missing.
+   * @param envName - The name of the environment variable.
+   * @returns True if the ENV is set and its value equals to 'true', or False otherwise.
+   */
+  getRequiredBooleanEnv(envName: string): boolean {
+    const envVal = this.getRequiredEnv(envName);
+
+    if (!/true|false/.test(envVal)) this.stopProcess(`Variable '${envName}' is not of boolean type.`);
+
+    return envVal === "true";
+  }
+
+  /**
+   * Retrieves the value of optional boolean environment variable.
    * If the env isn't set default value will be returned.
    * @param envName - The name of the environment variable.
+   * @param defaultValue - The default value for the case when the environment variable is not set.
+   * @returns True if the ENV is set and its value equals to 'true', or False otherwise; default value or undefined if the ENV is not set.
    */
   getOptionalBooleanEnv(envName: string): boolean | undefined;
   getOptionalBooleanEnv(envName: string, defaultValue: boolean): boolean;
@@ -130,16 +143,70 @@ export class EnvService {
   }
 
   /**
-   * Returns true if environment variable was set and its value equals 'true'. Otherwise returns false.
-   * Throws an error if variable is not of boolean type.
+   * Retrieves the value of required environment variable and creates a URL.
+   * ENV value must be a valid URL string.
    * @param envName - The name of the environment variable.
+   * @returns The URL object, created from the ENV value.
    */
-  getRequiredBooleanEnv(envName: string): boolean {
+  getRequiredURL(envName: string): URL {
     const envVal = this.getRequiredEnv(envName);
-    if (envVal === "skip_variable") return false;
-    if (!/true|false/.test(envVal)) this.stopProcess(`Variable '${envName}' is not of boolean type.`);
 
-    return envVal === "true";
+    try {
+      return new URL(envVal);
+    } catch (_) {
+      this.stopProcess(`Variable '${envName}' must be a valid URL`);
+    }
+  }
+
+  /**
+   * Retrieves the value of optional environment variable and creates a URL.
+   * ENV value must be a valid URL string.
+   * @param envName - The name of the environment variable.
+   * @returns The URL object, created from the ENV value, default value, or undefined.
+   */
+  getOptionalURL(envName: string): URL | undefined;
+  getOptionalURL(envName: string, defaultValue: URL): URL;
+  getOptionalURL(envName: string, defaultValue?: URL): URL | undefined {
+    const envVal = this.getOptionalEnv(envName);
+
+    try {
+      return envVal ? new URL(envVal) : defaultValue;
+    } catch (_) {
+      this.stopProcess(`Variable '${envName}' must be a valid URL`);
+    }
+  }
+
+  /**
+   * Converts a Time Period value from an Env into number according to "outTimeIn" option.
+   * Input value must be in format: <number><timeFormat>, where 'timeFormat' is one of: "ms" | "s" | "m" | "h" | "d".
+   * "timeFormat" is optional and has default value "ms".
+   * @param envName - The name of the environment variable.
+   * @param defaultValue - The default time period in the acceptable format.
+   * @param outTimeIn - The default time period in the acceptable format. [Default: "ms"].
+   * @returns - The numbered time period.
+   */
+  getOptionalTimePeriod(envName: string, defaultValue: string, outTimeIn: TimeFormat = "ms"): number {
+    const val = process.env[envName] || defaultValue;
+
+    if (!/^\d+ *[(?=.*s)(?=.*m)(?=.*h)(?=.*d)]{0,1}$/i.test(val))
+      this.stopProcess(
+        `Variable '${envName}' is not in the acceptable format. It must be: <number><"ms"|"s"|"m"|"h"|"d">. Ex.: '12h', '2d', '2D', '2 d'`,
+      );
+
+    const [, numb, pointer] = /^(\d+) *([(?=.*s)(?=.*m)(?=.*h)(?=.*d)]{0,1})$/i.exec(val) ?? [];
+    const timePointer = (pointer?.toLowerCase() || "ms") as TimeFormat;
+
+    const mapper: Record<TimeFormat, number> = {
+      ms: 1,
+      s: 1_000,
+      m: 60_000,
+      h: 3_600_000,
+      d: 86_400_000,
+    };
+
+    const multiplier = outTimeIn === timePointer ? 1 : mapper[timePointer] / mapper[outTimeIn];
+
+    return Number(numb) * multiplier;
   }
 
   /**
@@ -250,76 +317,5 @@ export class EnvService {
     }
 
     return parsedArray as R extends any[] ? R : R[];
-  }
-
-  /**
-   * Converts a Time Period value from an Env into number according to "outTimeIn" option.
-   * Input value must be in format: <number><timeFormat>, where 'timeFormat' is one of: "ms" | "s" | "m" | "h" | "d".
-   * "timeFormat" is optional and has default value "ms".
-   * @param envName - The name of the environment variable.
-   * @param defaultValue - The default time period in the acceptable format.
-   * @param outTimeIn - The default time period in the acceptable format. [Default: "ms"].
-   * @returns - The numbered time period.
-   */
-  getTimePeriod(envName: string, defaultValue: string, outTimeIn: TimeFormat = "ms"): number {
-    const val = process.env[envName] || defaultValue;
-
-    if (!/^\d+ *[(?=.*s)(?=.*m)(?=.*h)(?=.*d)]{0,1}$/i.test(val))
-      this.stopProcess(
-        `Variable '${envName}' is not in the acceptable format. It must be: <number><"ms"|"s"|"m"|"h"|"d">. Ex.: '12h', '2d', '2D', '2 d'`,
-      );
-
-    const [, numb, pointer] = /^(\d+) *([(?=.*s)(?=.*m)(?=.*h)(?=.*d)]{0,1})$/i.exec(val) ?? [];
-    const timePointer = (pointer?.toLowerCase() || "ms") as TimeFormat;
-
-    const mapper: Record<TimeFormat, number> = {
-      ms: 1,
-      s: 1_000,
-      m: 60_000,
-      h: 3_600_000,
-      d: 86_400_000,
-    };
-
-    const multiplier = outTimeIn === timePointer ? 1 : mapper[timePointer] / mapper[outTimeIn];
-
-    return Number(numb) * multiplier;
-  }
-
-  /**
-   * Gets the URL Env.
-   * Env value must be a valid URL string.
-   * @param envName - The name of the environment variable.
-   * @returns - The URL href string.
-   */
-  getRequiredURL(envName: string): string {
-    const envVal = this.getRequiredEnv(envName);
-
-    // skip envs flow
-    if (envVal === SKIP_ENV) return `http://${SKIP_ENV}.com`;
-
-    try {
-      return new URL(envVal).href;
-    } catch (_) {
-      this.stopProcess(`'${envName}' must be a valid URL`);
-    }
-  }
-
-  /**
-   * Gets the URL from optional Env.
-   * If the Env is unset undefined will be returned.
-   * @param envName - The name of the environment variable.
-   * @returns - The URL href from the Env or default value or undefined.
-   */
-  getOptionalURL(envName: string): string | undefined;
-  getOptionalURL(envName: string, defaultValue: string): string;
-  getOptionalURL(envName: string, defaultValue?: string): string | undefined {
-    const envVal = this.getOptionalEnv(envName) ?? defaultValue;
-    console.log("booo");
-
-    try {
-      return envVal ? new URL(envVal).href : undefined;
-    } catch (_) {
-      this.stopProcess(`'${envName}' must be a valid URL`);
-    }
   }
 }
